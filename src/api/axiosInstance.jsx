ee88ceefb7274cb3b403683b047b9ee261b24b6c
AndresR08 <andresr22ro@gmail.com>
2025-03-13 04:27:27 -0500

    Actualización de axiosInstance.js para usar IP de AWS en desarrollo

--- src/api/axiosInstance.jsx
+++ src/api/axiosInstance.jsx
@@ -1,25 +1,22 @@
 import axios from "axios";
 
+const baseURL =
+  import.meta.env.MODE === "development"
+    ? "http://3.142.130.175/api" 
+    : "/api"; // En producción, Netlify usará su proxy
+
 const api = axios.create({
-  baseURL: "/api", // Eliminamos la IP para que use el proxy de Netlify
+  baseURL,
   headers: { "Content-Type": "application/json" },
 });
-
-const token = localStorage.getItem("token");
-
-if (token) {
-  api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-}
 
 // Agregar el token en cada petición
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
-
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
-
     return config;
   },
   (error) => Promise.reject(error)
