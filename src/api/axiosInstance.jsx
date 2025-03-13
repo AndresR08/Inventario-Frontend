a45ab6aedc741d2fcf830e0e37f42b39ac9c3202
AndresR08 <andresr22ro@gmail.com>
2025-03-13 01:41:28 -0500

    Configuración de CORS y ajustes en axiosInstance.js

--- src/api/axiosInstance.jsx
+++ src/api/axiosInstance.jsx
@@ -1,16 +1,11 @@
 import axios from "axios";
 
-const apiBaseUrl = "http://3.142.130.175:5000"; // Usa HTTP por ahora
-
 const api = axios.create({
-  baseURL: apiBaseUrl,
+  baseURL: "/api", // Eliminamos la IP para que use el proxy de Netlify
   headers: { "Content-Type": "application/json" },
 });
 
-
-
 const token = localStorage.getItem("token");
-console.log("Token en localStorage:", token);
 
 if (token) {
   api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
@@ -20,15 +15,11 @@ if (token) {
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
-    console.log("Token en interceptor:", token);
 
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
-    } else {
-      console.warn("⚠ No hay token en localStorage");
     }
 
-    console.log("Headers antes de la petición:", config.headers);
     return config;
   },
   (error) => Promise.reject(error)
