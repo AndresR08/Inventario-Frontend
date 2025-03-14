f6a25d848895401431a630df88db3ee5a856a5a9
AndresR08 <andresr22ro@gmail.com>
2025-03-14 03:41:04 -0500

    Configuración de red en VM y actualización de API_URL

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const API_URL = "http://18.227.102.242:5000/api"; // URL base para el backend en AWS
+export const API_URL = "http://192.168.161.20:5000/api"; // URL base para el backend en AWS
 export const FRONTEND_URL = "http://localhost:3000"; // URL del frontend para pruebas locales
