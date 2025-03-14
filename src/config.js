905924f72bca0887bef215e982bd77084d13e6db
AndresR08 <andresr22ro@gmail.com>
2025-03-14 03:21:01 -0500

    Corregido duplicate export en config.js: Se exportan API_URL y FRONTEND_URL correctamente

--- src/config.js
+++ src/config.js
@@ -1,4 +1,2 @@
 export const API_URL = "http://18.227.102.242:5000/api"; // URL base para el backend en AWS
-const FRONTEND_URL = "http://localhost:3000"; // URL del frontend para pruebas locales
-
-export { API_URL, FRONTEND_URL };
+export const FRONTEND_URL = "http://localhost:3000"; // URL del frontend para pruebas locales
